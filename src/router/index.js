75ead85be9b0d6010b40ba45aaf8bdbdba6ef206
pirapat jitcharoenwirakun <pirapat.j@ku.th>
2021-07-26 21:06:48 +0700

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Income from '../views/Income.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Income
   },
   {
     path: '/about',
