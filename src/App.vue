9e982547d5c89ea9acc5fea47a5f375a7afcdbce
pirapat jitcharoenwirakun <pirapat.j@ku.th>
2021-07-26 21:59:28 +0700

    debug BG

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,13 @@
 </template>
 
 <style lang="scss">
+
+body{
+  min-height: 100vh;
+   background-image: url("https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm222-mind-22_1_2.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=3d5d85909cafb6e0f0de9905cf40ec01");
+  background-size: cover;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
